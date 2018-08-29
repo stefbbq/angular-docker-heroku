fb58ab3fa3ca0c5295f10012884601a6147c8343
stefansoc <stefan.soc@gmail.com>
2018-08-29 15:21:59 -0400

    CI setup

---
+++ entrypoint.sh
@@ -0,0 +1,13 @@
+set -e
+config_file="/etc/nginx/conf.d/default.conf"
+
+if [[ -z "$PORT" ]] ; then
+  echo "Error: PORT not defined!"
+  exit 0
+fi
+
+search_query="80"
+grep "$search_query" "$config_file"
+sed -i "s,$search_query,$PORT,g" "$config_file"
+
+nginx -g 'daemon off;'
